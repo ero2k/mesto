ee472f7e6d554e6c654d109171970680b0039de1
Denis Shchetinkin <evol.deluxe@yandex.ru>
2020-09-09 22:25:31 +0700

    add first card & popup new-place

--- js/script.js
+++ js/script.js
@@ -6,11 +6,38 @@ const inputProfession = popupContainer.querySelector('.popup__input_js_professio
 const buttonSave = popupContainer.querySelector('.popup__button-save');
 const buttonPopupClose = popupContainer.querySelector('.popup__button-close');
 
-
 const editProfileButton = document.querySelector('.profile__edit');
 
 const profileName = document.querySelector('.profile__title');
 const profileProffesional = document.querySelector('.profile__subtitle');
+
+const places = document.querySelector('.places');
+
+const initialCards = [{
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
 
 
 function editProfile() {
@@ -36,6 +63,20 @@ function closePopupByClickOverlay() {
         editProfile();
     }
 }
+
+function addPlace(img, title) {
+
+    const placeTemplate = document.querySelector('#placecard').content;
+    const placeElement = placeTemplate.cloneNode(true);
+
+    placeElement.querySelector('.place__photo').src = img;
+    placeElement.querySelector('.place__title').textContent = title;
+
+    places.append(placeElement);
+}
+
+
+initialCards.forEach(item => addPlace(item.link, item.name))
 
 buttonPopupClose.addEventListener('click', editProfile);
 
