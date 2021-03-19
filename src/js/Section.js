165cf534e5848565656cd0dc13a9d2157953ef8b
Denis Shchetinkin <evol.deluxe@yandex.ru>
2021-03-19 19:34:30 +0700

    ad

--- src/js/Section.js
+++ src/js/Section.js
@@ -1,19 +1,22 @@
 export default class Section {
-    constructor({items,renderer}, selector){
+  constructor({
+    items,
+    renderer
+  }, selector) {
     this._renderedItems = items;
     this._renderer = renderer;
     this._container = selector;
-    }
+  }
 
-    setItem(element) {
-        this._container.prepend(element);
-      }
+  setItem(element) {
+    this._container.prepend(element);
+  }
 
-    renderItems(fun) {
-      fun.then(res => res.json()).then(data => console.log(data))
-      // fun.forEach(item => {
-      //   console.log(item)
-      //     // this._renderer(item);
-      //   });
-      }
+  renderItems(fun) {
+    fun.then(res => res.json()).then(data => console.log(data))
+    // fun.forEach(item => {
+    //   console.log(item)
+    //     // this._renderer(item);
+    //   });
+  }
 }
